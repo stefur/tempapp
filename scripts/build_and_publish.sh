94f9945b041d2e0bfc4a1e8e92572b3b4e3ad147
Stefan Furne <stefan@furne.net>
2025-04-09 12:45:13 +0200

    Push first

--- scripts/build_and_publish.sh
+++ scripts/build_and_publish.sh
@@ -12,6 +12,9 @@ nix build .#packages.aarch64-linux.image -o arm64
 docker load < x86_64
 docker load < arm64
 
+docker push $REPOSITORY:$TAG-amd64
+docker push $REPOSITORY:$TAG-armd64
+
 docker manifest create $REPOSITORY:$TAG $REPOSITORY:$TAG-amd64 $REPOSITORY:$TAG-arm64
 docker manifest annotate $REPOSITORY:$TAG $REPOSITORY:$TAG-amd64 --arch amd64
 docker manifest annotate $REPOSITORY:$TAG $REPOSITORY:$TAG-arm64 --arch arm64
